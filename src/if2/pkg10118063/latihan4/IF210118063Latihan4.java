3034341b23c4619ced197088a201788e5f35c987
NaufalAshaW <55681676+NaufalAshaW@users.noreply.github.com>
2019-09-23 19:52:52 +0700

    Add files via upload

---
+++ src/if2/pkg10118063/latihan4/IF210118063Latihan4.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if2.pkg10118063.latihan4;
+
+/**
+ *
+ * @author 
+ * NAMA   : Naufal Asha
+ * KELAS  : PBO2
+ * NIM    : 10118063
+ */
+public class IF210118063Latihan4 {
+    public void tambahKambing() {
+        //Deklarasi Jumlah Kambing
+        int jumlahKambing = 0;
+        
+        jumlahKambing = jumlahKambing + 5;
+        System.out.println("Jumlah Kambing Setelah Ditambah: "+jumlahKambing);
+        
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        IF210118063Latihan4 kambingJantan = new IF210118063Latihan4();
+        kambingJantan.tambahKambing();
+    }
+    
+}
